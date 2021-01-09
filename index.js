2c91e0c6bc829971147de0d07231397daee62e0d
Erick Velasco <fervel1704@gmail.com>
2021-01-09 01:10:10 -0400

    git ignore and database created

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("./src/server/database.js")
+
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
+//const mongoose = require('mongoose')
 app.set('port', process.env.PORT || 8000)
 app.listen(app.get('port'), () => {
     console.log('servidor en puerto', app.get('port'))
