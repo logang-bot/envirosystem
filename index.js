562d26ac753727c921eb0827f34cf73891eb5e20
erick <erick@pop-os.localdomain>
2021-01-09 00:15:28 -0400

    --

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 app.set('port', process.env.PORT || 8000)
 app.listen(app.get('port'), ()=> {
-    console.log('servidor en puerto', app.get('port'))
-})
-const mongoose = require('mongoose')
-const app = express()
-
-app.set('port', process.env.PORT || 8000)
-
-app.listen(app.get('port'), ()=>{
     console.log('servidor en puerto', app.get('port'))
 })
